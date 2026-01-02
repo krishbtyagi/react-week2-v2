1d5cbff7ed021a2f7b67fd83ff116844f3807632
Krish Bhollar Tyagi <krishbtyagi@gmail.com>
2026-01-02 23:42:39 +0530

    prop drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-  return <div>
-    <LightBulb />
-  </div>
-}
-
-function LightBulb(){
   const [bulbOn, setBulbOn] = useState(true);
 
-  return <div>
-    <BulbState bulbOn={bulbOn} />
-    <ToggleBulbState setBulbOn={setBulbOn} />
-  </div>
+  return (
+    <div>
+      <Light bulbOn={bulbOn} setBulbOn={setBulbOn} />
+    </div>
+  );
 }
 
-function BulbState(bulbOn){
-  return <div>
-    {bulbOn ? "Bulb On" : "Bulb Off"}
-  </div>
+function Light({ bulbOn, setBulbOn }) {
+  return (
+    <div>
+      <LightBulb bulbOn={bulbOn} />
+      <LightSwitch setBulbOn={setBulbOn} />
+    </div>
+  );
 }
 
-function ToggleBulbState(setBulbOn){
+function LightBulb({ bulbOn }) {
+  return (
+    <div>
+      {bulbOn ? "Bulb On" : "Bulb Off"}
+    </div>
+  );
+}
 
-  function toggle(){
-    setBulbOn(currentState => !currentState)
+function LightSwitch({ setBulbOn }) {
+  function toggle() {
+    setBulbOn(currentState => !currentState);
   }
-  
-  return <div>
-    <button onClick={toggle}>toggle bulb</button>
-  </div>
+
+  return (
+    <div>
+      <button onClick={toggle}>Toggle Bulb</button>
+    </div>
+  );
 }
-export default App
+
+export default App;
